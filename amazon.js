0ca2f8f97bf25eba4a2b7cbaaa8bbb6943aab75c
Brittany Tinsley <brittanytinsley@Brittanys-MBP.jetpack>
2020-03-24 19:30:15 -0400

    updated java, added css page

--- amazon.js
+++ amazon.js
@@ -3,7 +3,10 @@ let basePrice = prompt("What is the Base price");
 let blackFriday = prompt("Is it Black Friday?");
 let searchEngine = prompt("Did you find this product through a search engine");
 let compShop = prompt("Did you find this product on a different site?");
-let message= `The base price for a ${itemName} is ${basePrice}.` ;
+let message = `The base price for a ${itemName} is ${basePrice} `;
+let message1;
+let message2;
+let message3;
 let finalPrice;
 
 // sending the complete message back to user usinng alert
@@ -11,18 +14,16 @@ let finalPrice;
 // the blackfriday, searchEngine, compShop will affect the price
 
 if (blackFriday === "yes"){
-    message + "Since it's Black Friday, we will reduce the price by 25%.";
-    finalPrice= (basePrice - (basePrice * .25));
-}
-
+    message1= `Since it's Black Friday, we will reduce the price by 25%.`;
+    finalPrice = (basePrice - (basePrice * .25));
+} 
 if (searchEngine === "yes") {
-    message + "We will increase the price by 1% to pay the search engine.";
-    (finalPrice * 1.01); 
+    message2=`We will increase the price by 1% to pay the search engine.`;
+    finalPrice = (finalPrice * 1.01); 
 }
 if (compShop === "yes") {
-    message + "This purchaser is a comparison shopper, so we will reduce the price by 10%.";
-    (basePrice * .10);
+    message3= `This purchaser is a comparison shopper, so we will reduce the price by 10%.`;
+    finalPrice= (basePrice * .10);
 }
 
-
-alert(`${message} The final price is ${finalPrice}.`);
+alert(`${message} ${message1} ${message2} ${message3}The final price is ${finalPrice}`)
